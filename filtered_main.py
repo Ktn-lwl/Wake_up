7fc9e366b58124431ec5fd06bb48a9931c1d5f28
Ktn-lwl <antirain.kitan@gmail.com>
2024-09-12 21:18:45 -0500

    Made time-based exec a function; added comments

--- filtered_main.py
+++ filtered_main.py
@@ -19,41 +19,64 @@ secret = "CLIENT_SECRET"
 redirect = "REDIRECT_URI"
 
 my_laptop = "MY_LAPTOP_NAME"
-exec_time = "13:20"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id = id, client_secret= secret,
                                                redirect_uri=redirect,
                                                scope=scp))
 
 class Device:
+    """
+    This helps me handle devices as objects with 2 attributes: a name, and an
+    ID
+    """
     def __init__(self, name: str, id: str):
         self.name = name
         self.id = id
 
 def get_devices():
+    """
+    Makes a call to the spotify API to return data on all currently active 
+    devices and then creates an instance of the Device class with it.
+    Returns a dictionary containing currently active devices with the
+    device names as keys.
+    """
     devices = {}
     for device in sp.devices()["devices"]:
         devices[f"{device['name']}"] = Device(device["name"], device["id"])
     return devices
 
 def get_time():
+    """
+    Returns a list with the current time (24hr clock) in the format: [hours, minutes]
+    """
     return [time.localtime().tm_hour, time.localtime().tm_min]
+
+def wait_till(exec_time):
+    """
+    Utilises the get_time() function defined above to keep the program in a loop
+    until the provided time, exec_time, is reached
+    """
+    now = get_time()
+
+    while f"{str(now[0]).rjust(2,'0')}:{str(now[1]).rjust(2,'0')}" != exec_time:
+        system("cls")
+        time.sleep(2)
+        print("Not yet...")
+        time.sleep(10)
+        now = get_time()
 
 active_devices = get_devices()
 
-while my_laptop not in active_devices.keys():
+while my_laptop not in active_devices:
+    """
+    Check if my laptop is a currently active device, and if it isn't, opens Spotify
+    via the command prompt
+    """
     system("spotify")
     time.sleep(5)
     active_devices = get_devices()
 
-print("DONE!")
-
-now = get_time()
-
-while f"{str(now[0]).rjust(2,'0')}:{str(now[1]).rjust(2,'0')}" != exec_time:
-    print("Not yet")
-    time.sleep(10)
-    now = get_time()
-
-print("Go time!")
+wait_till("21:08")
+system("cls")
+print("GO TIME!")
 
 sp.transfer_playback(active_devices[my_laptop].id)
